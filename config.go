9ac9127fb634f1b516a1c6d00583f219b4459cb1
Johan Sundell <johansundell@gmail.com>
2025-11-29 17:48:29 +0100

    refactor: Load application configuration from environment variables instead of a JSON file, using godotenv.

--- config.go
+++ config.go
@@ -1,40 +1,45 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"os"
-	"path/filepath"
+	"strconv"
 
 	"github.com/johansundell/template-service/types"
+	"github.com/joho/godotenv"
 )
-
-const filenameSettings = "settings.json"
 
 var settings types.AppSettings
 
 func init() {
-	ex, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using default/environment values")
 	}
 
-	dir, _ := filepath.Split(ex)
-	dat, err := os.ReadFile(dir + filenameSettings)
-	if err != nil {
-		settings = types.AppSettings{}
-		settings.Timeout = 15
+	settings = types.AppSettings{}
+
+	settings.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	settings.Port = os.Getenv("PORT")
+	if settings.Port == "" {
 		settings.Port = ":8080"
-		settings.UseFileSystem = false
-		data, err := json.Marshal(settings)
-		if err != nil {
-			log.Fatal("Could not write settings", err)
-		}
-		os.WriteFile(dir+filenameSettings, data, 0664)
-		log.Fatal("settings.json missing")
+	}
+	settings.UseFileSystem, _ = strconv.ParseBool(os.Getenv("USE_FILE_SYSTEM"))
+
+	timeoutStr := os.Getenv("TIMEOUT")
+	if timeoutStr != "" {
+		settings.Timeout, _ = strconv.Atoi(timeoutStr)
+	} else {
+		settings.Timeout = 15
 	}
 
-	if err := json.Unmarshal(dat, &settings); err != nil {
-		log.Fatal(err)
-	}
+	settings.UseMysql, _ = strconv.ParseBool(os.Getenv("USE_MYSQL"))
+	settings.UseSqlite, _ = strconv.ParseBool(os.Getenv("USE_SQLITE"))
+	settings.AuthToken = os.Getenv("AUTH_TOKEN")
+
+	settings.MySqlSettings.Username = os.Getenv("MYSQL_USERNAME")
+	settings.MySqlSettings.Password = os.Getenv("MYSQL_PASSWORD")
+	settings.MySqlSettings.Host = os.Getenv("MYSQL_HOST")
+	settings.MySqlSettings.Port = os.Getenv("MYSQL_PORT")
+	settings.MySqlSettings.Database = os.Getenv("MYSQL_DATABASE")
 }
